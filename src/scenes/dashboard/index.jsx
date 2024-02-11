d518df90d12e24f271a8de92dc7998a4591a05a3
Nefeli Karanikola <nefikaranikola@gmail.com>
2024-02-11 19:08:48 +0100

    initial commit

---
+++ src/scenes/dashboard/index.jsx
@@ -0,0 +1,15 @@
+import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+import Header from "../../components/Header";
+
+const Dashboard = () => {
+  return (
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subtitle="Welcome to your Dashboard" />
+      </Box>
+    </Box>
+  );
+};
+
+export default Dashboard;
---
+++ src/scenes/global/TopBar.jsx
@@ -0,0 +1,60 @@
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material/InputBase";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+
+const TopBar = () => {
+  const theme = useTheme();
+  //pass in colors, tokens and theme
+  //every time we need to use the color code that's in material UI we grab it from useTheme
+  //and pass it into tokens theme that we have created in the theme.js
+  const colors = tokens(theme.palette.mode);
+  //allows toggle change
+  const colorMode = useContext(ColorModeContext);
+
+  return (
+    //Box = Div
+    //Box allows css properties to be directly added in the components tag
+    <Box display="flex" justifyContent="space-between" p={2}>
+      {/*Search bar*/}
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchIcon />
+        </IconButton>
+      </Box>
+
+      {/*Icons*/}
+      <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <DarkModeOutlinedIcon />
+          ) : (
+            <LightModeOutlinedIcon />
+          )}
+        </IconButton>
+        <IconButton>
+          <NotificationsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <PersonOutlinedIcon />
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
+
+export default TopBar;
